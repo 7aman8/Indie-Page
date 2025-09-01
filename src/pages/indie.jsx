3cae0b2cb10694b38e2fe67682cff948346d5808
Abdulrahman Janahi <arjanahi2008@gmail.com>
2025-09-01 16:06:33 +0300

    made some changes to deplow

--- src/pages/indie.jsx
+++ src/pages/indie.jsx
@@ -6,9 +6,9 @@ import PaymentsIcon from '@mui/icons-material/Payments';
 import FmdGoodIcon from '@mui/icons-material/FmdGood';
 
 // Socials
-import X from '@mui/icons-material/X';
-import Github from '@mui/icons-material/Github';
-import Instagram from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import GithubIcon from '@mui/icons-material/Github';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const startups = [
@@ -31,89 +31,89 @@ const startups = [
 function Home(){
 
     return(
-        <div className="flex flex-col sm:flex-row gap-10 text-[#2b2b28] bg-[#F2EFE7]/70 min-h-screen w-full overflow-x-hidden">
+        <div className="flexIcon flexIcon-col sm:flexIcon-row gap-10 texIcont-[#2b2b28] bg-[#F2EFE7]/70 min-h-screen w-full overflow-xIcon-hidden">
 
             {/* Main page with picture */}
-            <div className=" sm:translate-x-20 sm:translate-y-20
-            flex-col gap-0 ml-10
+            <div className=" sm:translate-xIcon-20 sm:translate-y-20
+            flexIcon-col gap-0 ml-10
             ">
 
                 <img src={Profile} alt="Profile Picture" 
                 className="w-20 h-20 mt-5 sm:w-45 sm:h-45 rounded-full"
                 />
 
-                <h1 className="text-3xl italic font-[800] mt-5">Abdulrahman Janahi</h1>
+                <h1 className="texIcont-3xIconl italic font-[800] mt-5">Abdulrahman Janahi</h1>
 
-                <div className="flex align-middle items-center flex-row gap-5 text-gray-600 font-bold
+                <div className="flexIcon align-middle items-center flexIcon-row gap-5 texIcont-gray-600 font-bold
                 mt-4
                 ">
                     <span className="italic">
-                        <FmdGoodIcon className="w-6 h-6 -translate-y-[1px]" /> Bahrain 🇧🇭
+                        <FmdGoodIcon className="w-6 h-6 -translate-y-[1pxIcon]" /> Bahrain 🇧🇭
                     </span>
 
                     <span className="italic">
-                        <PaymentsIcon className="w-6 h-6 mr-[2px]" /> $0/month
+                        <PaymentsIcon className="w-6 h-6 mr-[2pxIcon]" /> $0/month
                     </span>
                 </div>
 
-                <div className="flex flex-col gap-2 mt-10">
-                    <span className="text-lg text-gray-600 italic">
+                <div className="flexIcon flexIcon-col gap-2 mt-10">
+                    <span className="texIcont-lg texIcont-gray-600 italic">
                         "Building products in public 🚀 | Developer"
                     </span>
                 </div>
 
                 {/* Socials */}
-                <div className="flex flex-row gap-10 mt-8 text-gray-600">
-                    <a href="https://x.com/Abdulrahma80860/" target="_blank" rel="noopener noreferrer"
+                <div className="flexIcon flexIcon-row gap-10 mt-8 texIcont-gray-600">
+                    <a href="https://xIcon.com/Abdulrahma80860/" target="_blank" rel="noopener noreferrer"
                     className="hover:scale-110 transition-transform duration-200"
                     >
-                        <X className="w-10 h-10" sx={{ fontSize: 30 }} />
+                        <XIcon className="w-10 h-10" sxIcon={{ fontSize: 30 }} />
                     </a>
-                    <a href="https://github.com/7aman8" target="_blank" rel="noopener noreferrer"
+                    <a href="https://githubIcon.com/7aman8" target="_blank" rel="noopener noreferrer"
                     className="hover:scale-110 transition-transform duration-200"
                     >
-                        <Github className="w-10 h-10" sx={{ fontSize: 30 }} />
+                        <GithubIcon className="w-10 h-10" sxIcon={{ fontSize: 30 }} />
                     </a>
-                    <a href="https://www.instagram.com/arj08._/" target="_blank" rel="noopener noreferrer"
+                    <a href="https://www.instagramIcon.com/arj08._/" target="_blank" rel="noopener noreferrer"
                     className="hover:scale-110 transition-transform duration-200"
                     >
-                        <Instagram className="w-10 h-10" sx={{ fontSize: 30 }} />
+                        <InstagramIcon className="w-10 h-10" sxIcon={{ fontSize: 30 }} />
                     </a>
                     <a href="https://www.linkedin.com/in/arj11/" target="_blank" rel="noopener noreferrer"
                     className="hover:scale-110 transition-transform duration-200"
                     >
-                        <LinkedInIcon className="w-10 h-10" sx={{ fontSize: 30 }} />
+                        <LinkedInIcon className="w-10 h-10" sxIcon={{ fontSize: 30 }} />
                     </a>
                 </div>  
 
             </div>
 
-            <hr className="block sm:hidden text-gray-400 mx-8 " />
+            <hr className="block sm:hidden texIcont-gray-400 mxIcon-8 " />
 
             {/* StartUps Page */}
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 m-10 sm:translate-x-25 ">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 m-10 sm:translate-xIcon-25 ">
 
-                {startups.map((startup, index) => (
+                {startups.map((startup, indexIcon) => (
                     <a
-                    key={index}
+                    key={indexIcon}
                     href={startup.link}
                     target="_blank"
-                    className="group h-25 rounded-box cursor-pointer p-5 duration-200 hover:scale-[1.02] hover:bg-zinc-100 
-                    shadow-sm bg-white rounded-2xl
+                    className="group h-25 rounded-boxIcon cursor-pointer p-5 duration-200 hover:scale-[1.02] hover:bg-zinc-100 
+                    shadow-sm bg-white rounded-2xIconl
                     "
                     >
                     {/* Logo and Name */}
-                    <div className="flex flex-wrap items-center gap-x-3 gap-y-1 lg:gap-x-4">
+                    <div className="flexIcon flexIcon-wrap items-center gap-xIcon-3 gap-y-1 lg:gap-xIcon-4">
                         <img
                         src={startup.logo}
                         alt={`${startup.name} Logo`}
                         className="object-cover duration-200 delay-100 group-hover:-rotate-12 h-6 w-6 lg:h-8 lg:w-8"
                         />
-                        <p className="mr-auto font-semibold lg:text-lg">{startup.name}</p>
+                        <p className="mr-auto font-semibold lg:texIcont-lg">{startup.name}</p>
                     </div>
 
                     {/* Description */}
-                    <p className="text-zinc-700/80 mt-2 text-sm lg:text-base">{startup.description}</p>
+                    <p className="texIcont-zinc-700/80 mt-2 texIcont-sm lg:texIcont-base">{startup.description}</p>
                     </a>
                 ))}
             </div>
