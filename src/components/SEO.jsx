95caed368f05791f858681a21642c017e3791d02
Abdulrahman Janahi <arjanahi2008@gmail.com>
2026-07-26 23:39:14 +0300

    The openGraph image should appear.

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -3,24 +3,30 @@ import React from 'react';
 const SEO = ({ title, description, path, image = "/Opengraph.png" }) => {
   const siteUrl = "https://www.arjbuilds.dev";
   const canonicalUrl = `${siteUrl}${path}`;
+  const normalizedPath = path.startsWith("/") ? path : `/${path}`;
+  const imageUrl = image.startsWith("http")
+    ? image
+    : `${siteUrl}${image.startsWith("/") ? image : `/${image}`}`;
   
   return (
     <>
       <title>{title}</title>
       <meta name="description" content={description} />
-      <link rel="canonical" href={canonicalUrl} />
+      <link rel="canonical" href={`${siteUrl}${normalizedPath}`} />
       
       {/* Open Graph */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:url" content={canonicalUrl} />
-      <meta property="og:image" content={`${siteUrl}${image}`} />
+      <meta property="og:url" content={`${siteUrl}${normalizedPath}`} />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:image:secure_url" content={imageUrl} />
+      <meta property="og:image:type" content="image/png" />
       
       {/* Twitter */}
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:url" content={canonicalUrl} />
-      <meta name="twitter:image" content={`${siteUrl}${image}`} />
+      <meta name="twitter:url" content={`${siteUrl}${normalizedPath}`} />
+      <meta name="twitter:image" content={imageUrl} />
     </>
   );
 };
